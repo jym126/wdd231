2bd28bf71015a890feaeda9dc7de159c4d0c90af
jym126 <jym126@hotmail.com>
2025-03-12 21:05:08 +0100

    directory page

--- chamber/scripts.js
+++ chamber/scripts.js
@@ -12,3 +12,49 @@ document.getElementById('current-year').textContent = currentYear;
 
 const lastModified = document.lastModified;
 document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
+
+//Directory page
+
+const container = document.getElementById('membersContainer');
+const gridButton = document.getElementById('gridView');
+const listButton = document.getElementById('listView');
+
+async function fetchMembers() {
+    try {
+        const response = await fetch('data/members.json');
+        const members = await response.json();
+        displayMembers(members);
+    } catch (error) {
+        console.error('Error fetching member data:', error);
+    }
+}
+
+function displayMembers(members) {
+    container.innerHTML = '';
+    members.forEach(member => {
+        const memberCard = document.createElement('div');
+        memberCard.className = 'member-card';
+        memberCard.innerHTML = `
+        <img src="images/${member.image}" alt="${member.name}">
+            <h3>${member.name}</h3>
+            <p>${member.address}</p>
+            <p>${member.phone}</p>
+            <a href="${member.website}" target="_blank">Visit Website</a>
+            <p>${member.description}</p>
+            <p>Membership Level: ${member.membershipLevel}</p>
+        `;
+        container.appendChild(memberCard);
+    });
+}
+
+gridButton.addEventListener('click', () => {
+    container.classList.remove('list');
+    container.classList.add('grid');
+});
+
+listButton.addEventListener('click', () => {
+    container.classList.remove('grid');
+    container.classList.add('list');
+});
+
+fetchMembers();
